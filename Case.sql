c200bccab572861dfd9ea5481997f928212bac65
mosesglitch <mosesglitch>
2023-01-19 11:30:23 +0300

    add case statements

---
+++ Case.sql
@@ -0,0 +1,78 @@
+The CASE statement always goes in the SELECT clause.
+-- CASE must include the following components: WHEN, THEN, and END. ELSE is an optional component to catch cases that didn’t meet any of the other previous CASE conditions.
+-- You can make any conditional statement using any conditional operator (like WHERE) between WHEN and THEN. This includes stringing together multiple conditional statements using AND and OR.
+-- You can include multiple WHEN statements, as well as an ELSE statement again, to deal with any unaddressed conditions.
+SELECT account_id, CASE WHEN standard_qty = 0 OR standard_qty IS NULL THEN 0
+                        ELSE standard_amt_usd/standard_qty END AS unit_price
+FROM orders
+LIMIT 10;
+
+SELECT id,
+       account_id,
+       occurred_at,
+       channel,
+       CASE WHEN channel = 'facebook' THEN 'yes' ELSE 'no' END AS is_facebook
+FROM web_events
+ORDER BY occurred_at
+
+SELECT id,
+       account_id,
+       occurred_at,
+       channel,
+       CASE WHEN channel = 'facebook' OR channel = 'direct' THEN 'yes' 
+       ELSE 'no' END AS is_facebook
+FROM web_events
+ORDER BY occurred_at
+
+SELECT account_id,
+       occurred_at,
+       total,
+       CASE WHEN total > 500 THEN 'Over 500'
+            WHEN total > 300 THEN '301 - 500'
+            WHEN total > 100 THEN '101 - 300'
+            ELSE '100 or under' END AS total_group
+FROM orders
+
+SELECT account_id,
+       occurred_at,
+       total,
+       CASE WHEN total > 500 THEN 'Over 500'
+            WHEN total > 300 AND total <= 500 THEN '301 - 500'
+            WHEN total > 100 AND total <=300 THEN '101 - 300'
+            ELSE '100 or under' END AS total_group
+FROM orders
+
+SELECT CASE WHEN total > 500 THEN 'OVer 500'
+            ELSE '500 or under' END AS total_group,
+            COUNT(*) AS order_count
+FROM orders
+GROUP BY 1
+
+SELECT CASE WHEN total >= 2000 THEN 'At Least 2000'
+WHEN total >= 1000 AND total < 2000 THEN 'Between 1000 and 2000'
+ELSE 'Less than 1000' END AS order_category,
+COUNT(*) AS order_count
+FROM orders
+GROUP BY 1;
+
+SELECT a.name, SUM(total_amt_usd) total_spent, 
+  CASE WHEN SUM(total_amt_usd) > 200000 THEN 'top'
+  WHEN  SUM(total_amt_usd) > 100000 THEN 'middle'
+  ELSE 'low' END AS customer_level
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id 
+GROUP BY a.name
+ORDER BY 2 DESC;
+
+SELECT a.name, SUM(total_amt_usd) total_spent, 
+  CASE WHEN SUM(total_amt_usd) > 200000 THEN 'top'
+  WHEN  SUM(total_amt_usd) > 100000 THEN 'middle'
+  ELSE 'low' END AS customer_level
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id
+WHERE occurred_at > '2015-12-31' 
+GROUP BY 1
+ORDER BY 2 DESC;
+
